66bd389665c88563c9a302db853a9bc41cc68653
BarreraJoel <joelmanuelbar@gmail.com>
2025-03-12 19:50:27 -0300

    refactor: :adhesive_bandage: arreglo config vite

--- routes/movies.php
+++ routes/movies.php
@@ -4,13 +4,14 @@ use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 
-
-Route::resource('movies', MovieController::class)
-->except(['show', 'edit']);
-
-Route::prefix('movies')->group(function () {
-    Route::get('{movie:slug}', [MovieController::class, 'show'])
-    ->name('movies.show');
-    Route::get('{movie:slug}/edit', [MovieController::class, 'edit'])
-    ->name('movies.edit');
+Route::middleware('auth')->group(function() {
+    Route::resource('movies', MovieController::class)
+    ->except(['show', 'edit']);
+    
+    Route::prefix('movies')->group(function () {
+        Route::get('{movie:slug}', [MovieController::class, 'show'])
+        ->name('movies.show');
+        Route::get('{movie:slug}/edit', [MovieController::class, 'edit'])
+        ->name('movies.edit');
+    });
 });
